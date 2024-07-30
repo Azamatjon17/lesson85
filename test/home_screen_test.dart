f52b159197d992167c9197454bfb20d7ff02d8ef
Azamatjon <azamatjonkomilov4@gmail.com>
2024-07-30 09:16:33 +0500

    fg

---
+++ test/home_screen_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lesson85/bloc/quiz_bloc.dart';
+import 'package:lesson85/bloc/quiz_event.dart';
+import 'package:lesson85/views/screens/home_screen.dart';
+
+class MockQuizBloc extends QuizBloc {}
+
+void main() {
+  late QuizBloc mockQuizBloc;
+
+  setUp(() {
+    mockQuizBloc = MockQuizBloc();
+  });
+
+  group('HomeScreen Tests', () {
+    testWidgets('renders Start button', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<QuizBloc>(
+            create: (_) => mockQuizBloc,
+            child: const HomeScreen(),
+          ),
+        ),
+      );
+
+      expect(find.text('Start'), findsOneWidget);
+    });
+
+    testWidgets('triggers FirsQuizEvent when Start button is tapped',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<QuizBloc>(
+            create: (_) => mockQuizBloc,
+            child: const HomeScreen(),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(FilledButton));
+      await tester.pump();
+    });
+  });
+}
